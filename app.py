434bb5f6c767c7079aaa16c0a8165dc796959099
bryansantosk <brayansantosxavier@gmail.com>
2025-07-08 16:12:07 -0300

    corrige o final do app.py para rodar no Render

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def quiz():
     return render_template('quiz.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from os import environ
+    app.run(host='0.0.0.0', port=int(environ.get("PORT", 5000)))
